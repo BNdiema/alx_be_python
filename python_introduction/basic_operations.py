30e852eb5cf26e9317779364c6568e9d64a7b172
BNdiema <bryantndiema@gmail.com>
2024-09-22 02:44:48 +0300

    fixed the variables spelling with caps case for first letter

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,9 +2,9 @@ from PIL.ImageChops import difference
 
 number1 = 10
 number2 = 5
-sum = number1 + number2
-print("Addition of", number1, "and", number2, "is", sum)
-difference = number1 - number2
-print("Subtraction of", number1, "and", number2, "is", difference)
-product = number1 * number2
-print("Multiplication of", number1, "and", number2, "is", product)
+Sum = number1 + number2
+print("Addition of", number1, "and", number2, "is", Sum)
+Difference = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", Difference)
+Product = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", Product)
