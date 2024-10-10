eef3817f81c15b8b6d8dbbddd8b7ac22288b0244
BNdiema <bryantndiema@gmail.com>
2024-10-10 11:25:21 +0300

    Created a Python script named arithmetic_operations.py. containing a function that performs basic arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,15 @@
 
+def perform_operation(num1, num2, operation):
+
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num1 and num2 == 0:
+                print("Cannot divide by zero")
+            else:
+                return num1 / num2
