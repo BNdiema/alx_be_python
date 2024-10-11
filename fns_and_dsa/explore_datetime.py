bd98a29d8d0d351e7c4391eb541652c1a64982b1
BNdiema <bryantndiema@gmail.com>
2024-10-12 01:53:59 +0300

    Added import of the datetime module to fileand fixed rediclearing within the function

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,14 @@
-from datetime import datetime, timedelta
+from _datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(current_date)
 
 
 def calculate_future_date():
     number_of_days = input("Enter number of days to add to the current date: ")
     number_of_days = int(number_of_days)
-    date_now = datetime.datetime.now()
-    future_date = date_now + datetime.timedelta(days=number_of_days)
-    print(f"Future date: {future_date}")
+    date_now = datetime.now()
+    future_date = date_now + timedelta(days=number_of_days)
+    print("Future date: ", future_date.strftime('%Y-%m-%d'))
+
