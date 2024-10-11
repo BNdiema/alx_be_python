379d05f16a74633e9e2366a67b226f5906fcd42b
BNdiema <bryantndiema@gmail.com>
2024-10-12 02:01:08 +0300

    added formating to how the dates and time are displayed in YYYY-MM-DD H:M:S

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(current_date)
+    print(current_date.strftime('%Y-%m-%-%d %H:%M:%S'))
 
 
 def calculate_future_date():
@@ -11,4 +11,3 @@ def calculate_future_date():
     date_now = datetime.now()
     future_date = date_now + timedelta(days=number_of_days)
     print("Future date: ", future_date.strftime('%Y-%m-%d'))
-
