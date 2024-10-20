2bc26ca5161240c67b6074933257105166c2fb18
BNdiema <bryantndiema@gmail.com>
2024-10-20 19:32:16 +0300

    Corrected the output for division by zero by removing 'ZeroDivisionError'

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
         if float(numerator) and float(denominator) == 0:
-            raise ZeroDivisionError("Error: Cannot divide by zero.")
+            raise "Error: Cannot divide by zero."
         else:
             result = float(numerator) / float(denominator)
             return f"The result of the division is {result}"
