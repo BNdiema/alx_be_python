b928734096d716d874cd44d0c20ac52cd6f3f3ff
BNdiema <bryantndiema@gmail.com>
2024-10-20 20:48:57 +0300

    Changed the syntax of all methods by removing the result variable

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,16 +7,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_add(self):
-        result = self.calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(self.calc.add(10, 5), 15)
 
     def test_subtract(self):
-        result = self.calc.subtract(5, 2)
-        self.assertEqual(result, 3)
+        self .assertEqual(self.calc.subtract(5, 2), 3)
 
     def test_multiply(self):
-        result = self.calc.multiply(4, 5)
-        self.assertEqual(result, 20)
+        self.assertEqual(self.calc.multiply(4, 5), 20)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
